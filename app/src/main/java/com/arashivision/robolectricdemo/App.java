ae23a2b582b8ddbdbc5392750e879ef0ec1ccc8b
changjiashuai <changjiashuai@gmail.com>
2017-02-17 11:27:19 +0800

    1. Config Robolectric finished.

---
+++ app/src/main/java/com/arashivision/robolectricdemo/App.java
@@ -0,0 +1,12 @@
+package com.arashivision.robolectricdemo;
+
+import android.app.Application;
+
+/**
+ * Email: changjiashuai@gmail.com
+ *
+ * Created by CJS on 2017/2/17 11:16.
+ */
+
+public class App extends Application {
+}
--- app/src/test/java/com/arashivision/robolectricdemo/MainActivityTest.java
+++ app/src/test/java/com/arashivision/robolectricdemo/MainActivityTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
+import static android.os.Build.VERSION_CODES.KITKAT;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -14,9 +16,11 @@ import static org.junit.Assert.assertTrue;
  */
 @RunWith(RobolectricTestRunner.class)
 //@Config(constants = BuildConfig.class, sdk = 22 /* 23,24,25 ng*/)
+@Config(application = App.class)
 public class MainActivityTest {
 
     @Test
+    @Config(sdk = KITKAT)
     public void testOnCreate() throws Exception {
         assertTrue(Robolectric.setupActivity(MainActivity.class) != null);
     }
