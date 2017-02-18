0e52bb0dc344f1c3bab9f5e5921a637bb47c997f
changjiashuai <changjiashuai@gmail.com>
2017-02-18 14:31:36 +0800

    找出冗余的互动（即未被验证到的）

--- app/src/test/java/com/arashivision/robolectricdemo/MockitoTest.java
+++ app/src/test/java/com/arashivision/robolectricdemo/MockitoTest.java
@@ -189,4 +189,24 @@ public class MockitoTest {
         //验证零互动行为
         verifyNoMoreInteractions(list2, list3);
     }
+
+    /**
+     * 找出冗余的互动（即未被验证到的）
+     */
+    @Test
+    public void testFindRedUndantInteraction(){
+        List list = mock(List.class);
+        list.add(1);
+        list.add(2);
+        verify(list, times(2)).add(anyInt());
+        //检查是否有未被验证的互动行为，因为add(1)和add(2)都会被上面的anyInt()验证到，所以下面的代码会通过
+        verifyNoMoreInteractions(list);
+
+        List list2 = mock(List.class);
+        list2.add(1);
+        list2.add(2);
+        verify(list2).add(1);
+        //检查是否有未被验证的互动行为，因为add(2)没有被验证，所以下面的代码会失败抛出异常
+//        verifyNoMoreInteractions(list2);
+    }
 }
