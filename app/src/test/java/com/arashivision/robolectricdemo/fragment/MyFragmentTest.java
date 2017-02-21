1aa7e11cb2fe95c28f72e504e7d2a9fda5a2485b
changjiashuai <changjiashuai@gmail.com>
2017-02-21 10:36:28 +0800

    android.app.Fragment 单元测试完成

---
+++ app/src/test/java/com/arashivision/robolectricdemo/fragment/MyFragmentTest.java
@@ -0,0 +1,32 @@
+package com.arashivision.robolectricdemo.fragment;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.util.FragmentTestUtil;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Email: changjiashuai@gmail.com
+ *
+ * Created by CJS on 2017/2/21 10:32.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class MyFragmentTest {
+
+    private MyFragment mMyFragment;
+
+    @Before
+    public void setup() throws Exception {
+        mMyFragment = new MyFragment();
+        // 把Fragment添加到Activity中 ---> android.app.Fragment
+        FragmentTestUtil.startFragment(mMyFragment);
+    }
+
+    @Test
+    public void testFragment() throws Exception {
+        assertNotNull(mMyFragment.getView());
+    }
+}
